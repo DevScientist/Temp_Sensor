954a261b7ef85584d335babbaec7634d6fac03e2
DevScientist <daltonbroka@gmail.com>
2017-06-05 11:27:48 -0500

    Clean-up, SMS Feature
    Cleaned up the code and added an SMS feature that sends a text message based on the Twilio module if the temperature of the freezer gets above a certain set point (in this case -13 degC)

--- temp_sensor.py
+++ temp_sensor.py
@@ -1,8 +1,9 @@
 import os
 import time
-import csv
-import io
 from time import strftime
+import csv
+import twilio
+from twilio.rest import Client
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -16,6 +17,7 @@ def temp_raw():
     return lines
 
 def read_temp():
+    global temp_c
     lines = temp_raw()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
@@ -26,14 +28,19 @@ def read_temp():
         temp_c = float(temp_string)/1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32
         return temp_c
-    
+
+
 while True:
-    print(read_temp())
-    text = str(read_temp())
+    read_temp()
+    print(temp_c)
     with open("temp.csv", 'a') as f:
         stamp = strftime('%B %d %Y, %I:%M %p')
         row = str(read_temp())
         w = csv.writer(f)
         w.writerow([row, stamp])
+    if temp_c > -13:
+        client = Client("", "")
+        client.messages.create(to="+1", 
+                               from_="+1", 
+                               body="Alarm- Home Freezer is %s degrees C" %temp_c)
     time.sleep(600)
-
